fc75ca87929834cf2fd9e82be568cd5012c02010
Licensed-Idiot <170021962+Licensed-Idiot@users.noreply.github.com>
2024-09-04 13:29:38 -0400

    Added install for AUR helper

---
+++ Install/installaur.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+read -p "Install AUR helper
+	[1] yay
+	[2] paru 
+	(default=1): " aur
+
+# set default option for AUR helper
+if ! [ $aur ]; then
+    aur="1"
+fi
+
+# if yay, install yay
+if [ $aur == "1" ]; then
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+
+# if paru install paru
+elif [ $aur == "2" ]; then
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg -si
+
+else
+    echo -e "Not a valid option"
+fi
