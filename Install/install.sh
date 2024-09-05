25d32cf44f97685c4c4b8cc9c52b9780ec774b14
Licensed-Idiot <170021962+Licensed-Idiot@users.noreply.github.com>
2024-09-05 14:00:25 -0400

    finished install script

--- Install/install.sh
+++ Install/install.sh
@@ -22,3 +22,32 @@ fi
 if [ $aur_helper ]; then
     $aur_helper -S --needed $(awk '{print $1}'  packages.lst)
 fi
+
+
+# if zsh installed, install Oh-My-Zsh
+if type -p zsh > /dev/null; then
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+    # install p10k
+    git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+    # install zsh plugins
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+# check if the configs files are present, copy the config files
+if [ -d "$HOME/dotfiles/Configs" ]; then
+    cp -r $HOME/dotfiles/Configs/. $HOME/
+else
+    echo -e "Could not find path to configs, make sure they are located in ~/dotfiles/Configs"
+fi
+
+# reboot the system
+read -n 1 -p "Reboot now? (Y/n): " reboot
+
+if [ $reboot == "Y" ] || [ $reboot == "y" ]; then
+    reboot
+else
+    echo "Reboot canceled, the dotfiles will not take effect unless you reboot"
+fi
